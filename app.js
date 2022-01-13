f5d2296b7e2d072475fb3901f2577f2552de908e
muhammadYus <74594824+muhammadYus@users.noreply.github.com>
2022-01-13 18:01:09 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+// let 
+// output = document.getElementById("output");
+// let 
+// input = +prompt("Введите число?");
+// for(let i = 1; i <= input; i++){
+//     output.innerHTML = i;
+// }   
+// Вот это примерно получилось;           
+// let input = +prompt("Число строк");
+// for(let i = 0 ; i < input; i++){
+//     for(let j = 0; j < i; j++){
+//         output.innerHTML += "*";
+
+//     }
+//     output.innerHTML += "<br>";
+// }
+// А это ни фига ниче не вышло;
+// let input = +prompt("Число строк");
+// for(let i = 0 ; i < input; i++){
+//     for(let j = 11; j == i; console.log(j = i) ){
+//         output.innerHTML += "*";
+//     }
+//     output.innerHTML += "<br>";
+// }
+//  let input = +prompt("Число строк");
+//  for(let i = input ; i == input; i--){
+//      for(let j = input; j == i; j--){
+//          output.innerHTML += "*";
+
+//      }
+//      output.innerHTML += "<br>";
+//  }
